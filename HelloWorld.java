eb57916085f6f954712efea4a52756902533bf8d
bhartshorn <brandon.hartshorn@gmail.com>
2015-10-09 21:35:15 -0600

    Added Hello World for 04.6

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+/*
+ * Program: Hello World
+ * @author: Brandon Hartshorn
+ */
+
+public class HelloWorld {
+   public static void main(String[] args) {
+      System.out.println("Hello World!");
+   }
+}
